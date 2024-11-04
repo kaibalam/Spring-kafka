65f940196c6afe7919639f73d3d56e5c9e512beb
Ricardo Pablo Bernardo Valenzuela Arévalo <kaibalam@hotmail.com>
2024-11-04 10:19:33 -0600

    Spring project docker-compose

---
+++ SpringBootProvider/src/main/java/com/kafka/provider/SpringBootProvider/SpringBootProviderApplication.java
@@ -0,0 +1,22 @@
+package com.kafka.provider.SpringBootProvider;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@SpringBootApplication
+public class SpringBootProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProviderApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(KafkaTemplate<String, String> kafkaTemplate){
+		return args -> {
+			kafkaTemplate.send("test-topic", "Prueba final de Springboot con kafka en docker");
+		};
+	}
+}
